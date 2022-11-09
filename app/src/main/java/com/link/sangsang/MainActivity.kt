3bb5e97a3de88831531b38aee3834f7b4f5ec8db
ohjun99 <ohjun1999@gmail.com>
2022-11-09 18:56:34 +0900

    2022_11_09_first

---
+++ app/src/main/java/com/link/sangsang/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.link.sangsang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import com.link.sangsang.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    // 전역 변수로 바인딩 객체 선언
+    private var mBinding: ActivityMainBinding? = null
+
+    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
+    private val binding get() = mBinding!!
+    private var doubleBackToExit = false
+
+    //    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 자동 생성된 뷰 바인딩 클래스에서의 inflate라는 메서드를 활용해서
+        // 액티비티에서 사용할 바인딩 클래스의 인스턴스 생성
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+//firestore 변수 초기화
+
+        setContentView(binding.root)
+    }
+
+    // 액티비티가 파괴될 때..
+    override fun onDestroy() {
+        // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 한다.
+        mBinding = null
+        super.onDestroy()
+    }
+
+
+    override fun onBackPressed() {
+        if (doubleBackToExit) {
+            finishAffinity()
+        } else {
+            Toast.makeText(this, "종료하시려면 뒤로가기를 한번 더 눌러주세요.", Toast.LENGTH_SHORT).show()
+            doubleBackToExit = true
+            runDelayed(1500L) {
+                doubleBackToExit = false
+            }
+        }
+    }
+
+
+    fun runDelayed(millis: Long, function: () -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed(function, millis)
+    }
+
+
+}
